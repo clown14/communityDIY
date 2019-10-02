11661b6879c63e50e52bf29a5ba81a7cc779a39a
clown14 <785613198@qq.com>
2019-10-02 15:40:56 +0800

    add lombok && fix

--- src/main/java/life/majiang/community2/dto/GithubUser.java
+++ src/main/java/life/majiang/community2/dto/GithubUser.java
@@ -1,48 +1,20 @@
 package life.majiang.community2.dto;
 
+import lombok.Data;
+
 /**
  * @program: community2
  * @author: onion
  * @create: 2019-09-18 22:50
  **/
 
+@Data
 public class GithubUser {
     private String name;
     private Long id;
     private String bio;
+    private String avatar_url;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
 }
 
     
--- src/main/java/life/majiang/community2/model/User.java
+++ src/main/java/life/majiang/community2/model/User.java
@@ -1,11 +1,14 @@
 package life.majiang.community2.model;
 
+import lombok.Data;
+
 /**
  * @program: community2
  * @author: onion
  * @create: 2019-09-19 19:10
  **/
 
+@Data
 public class User {
     private Integer id;
     private String name;
@@ -13,54 +16,7 @@ public class User {
     private String token;
     private Long gmtCreate;
     private Long gmtModified;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Long gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Long getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Long gmtModified) {
-        this.gmtModified = gmtModified;
-    }
+    private String avatarUrl;
 }
 
     
