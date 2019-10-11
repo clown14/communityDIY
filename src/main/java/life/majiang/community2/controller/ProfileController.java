a6968e2ef598c50d086529c343345ac1227b5f2e
clown14 <785613198@qq.com>
2019-10-11 23:03:43 +0800

    add profile page

---
+++ src/main/java/life/majiang/community2/controller/ProfileController.java
@@ -0,0 +1,69 @@
+package life.majiang.community2.controller;
+
+import life.majiang.community2.dto.PaginationDTO;
+import life.majiang.community2.mapper.UserMapper;
+import life.majiang.community2.model.User;
+import life.majiang.community2.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @program: community2
+ * @author: onion
+ * @create: 2019-10-11 22:56
+ **/
+
+@Controller
+public class ProfileController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable(name = "action") String action,
+                          @RequestParam(name = "page",defaultValue = "1") Integer page,
+                          @RequestParam(name = "size",defaultValue = "5") Integer size,
+                          HttpServletRequest request,
+                          Model model) {
+        Cookie[] cookies = request.getCookies();
+        User user = null;
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (user == null) {
+            return "redirect:/";
+        }
+
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "我的提问");
+        } else if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
+        }
+
+        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+        model.addAttribute("pagination", paginationDTO);
+        return "profile";
+    }
+}
+    
