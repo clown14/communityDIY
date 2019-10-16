0201b870780d6313256d0aebd63b66409c0ced85
clown14 <785613198@qq.com>
2019-10-16 22:46:13 +0800

    add interceptor

--- src/main/java/life/majiang/community2/controller/ProfileController.java
+++ src/main/java/life/majiang/community2/controller/ProfileController.java
@@ -34,20 +34,22 @@ public class ProfileController {
                           @RequestParam(name = "size",defaultValue = "5") Integer size,
                           HttpServletRequest request,
                           Model model) {
-        Cookie[] cookies = request.getCookies();
-        User user = null;
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+//        Cookie[] cookies = request.getCookies();
+//        User user = null;
+//        if (cookies != null && cookies.length != 0) {
+//            for (Cookie cookie : cookies) {
+//                if (cookie.getName().equals("token")) {
+//                    String token = cookie.getValue();
+//                    user = userMapper.findByToken(token);
+//                    if (user != null) {
+//                        request.getSession().setAttribute("user", user);
+//                    }
+//                    break;
+//                }
+//            }
+//        }
+
+        User user = (User) request.getSession().getAttribute("user");
 
         if (user == null) {
             return "redirect:/";
---
+++ src/main/java/life/majiang/community2/interceptor/WebConfig.java
@@ -0,0 +1,28 @@
+package life.majiang.community2.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: community2
+ * @author: onion
+ * @create: 2019-10-12 22:17
+ **/
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+    }
+}
+
+    
