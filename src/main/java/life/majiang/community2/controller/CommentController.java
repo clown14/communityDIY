2b57e88ecdcf8a73892af8d890bc8626cc2a9d80
clown14 <785613198@qq.com>
2019-11-16 23:46:19 +0800

    fix comment

--- src/main/java/life/majiang/community2/controller/CommentController.java
+++ src/main/java/life/majiang/community2/controller/CommentController.java
@@ -6,6 +6,7 @@ import life.majiang.community2.exception.CustomizeErrorCode;
 import life.majiang.community2.model.Comment;
 import life.majiang.community2.model.User;
 import life.majiang.community2.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,6 +35,10 @@ public class CommentController {
         User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
+        }
+
+        if (commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())) {
+            return ResultDTO.errorOf(CustomizeErrorCode.COMMENT_IS_EMPTY);
         }
 
         Comment comment = new Comment();
